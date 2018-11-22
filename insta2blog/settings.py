5d1433d9f83ba79c0e32f702c7da763266236f31
Ali Khundmiri <salikhundmiri@gmail.com>
2018-11-22 23:57:46 +0530

    debug

--- insta2blog/settings.py
+++ insta2blog/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'd&^k@d9qatlj)6x&7n0stnzm^jowf25_b@=gh-r8q0h^8n+ryy')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-PRODUCTION = False
+DEBUG = False
+PRODUCTION = True
 
 ALLOWED_HOSTS = [
 'https://insta2blog.herokuapp.com',
